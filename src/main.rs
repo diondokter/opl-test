a5f442475398e8b8555a92adeebb9cb32976d2b4
Dion Dokter <diondokter@gmail.com>
2020-09-23 01:18:26 +0200

    Can now blink a led

--- src/main.rs
+++ src/main.rs
@@ -3,15 +3,20 @@
 
 use cortex_m_rt::{exception, ExceptionFrame};
 use rtt_target::{rprintln, rtt_init, set_print_channel};
+use stm32f4xx_hal::{gpio::OpenDrain, gpio::Output, gpio::gpioa::PA6, gpio::gpioa::PA7, timer::Timer};
 use stm32f4xx_hal::{prelude::*, stm32::TIM4};
-use stm32f4xx_hal::timer::Timer;
 
 mod helpers;
+
+type Led2Pin = PA6<Output<OpenDrain>>;
+type Led3Pin = PA7<Output<OpenDrain>>;
 
 #[rtic::app(device = stm32f4xx_hal::stm32, peripherals = true, monotonic = rtic::cyccnt::CYCCNT)]
 const APP: () = {
     struct Resources {
         global_timer: Timer<TIM4>,
+        led_2: Led2Pin,
+        led_3: Led3Pin,
     }
 
     #[init()]
@@ -37,36 +42,51 @@ const APP: () = {
         rprintln!("Initializing!");
 
         let rcc = dp.RCC.constrain();
-        let clocks = rcc.cfgr.use_hse(8.mhz()).sysclk(168.mhz()).freeze();
+        let clocks = rcc
+            .cfgr
+            .use_hse(8.mhz())
+            .sysclk(168.mhz())
+            .hclk(168.mhz())
+            .pclk1(42.mhz())
+            .pclk2(84.mhz())
+            .freeze();
 
-        let mut global_timer = Timer::tim4(dp.TIM4, 1u32.hz(), clocks);
+        let gpioa = dp.GPIOA.split();
+
+        let mut led_2: Led2Pin = gpioa.pa6.into_open_drain_output();
+        led_2.set_high().unwrap();
+        let mut led_3: Led3Pin = gpioa.pa7.into_open_drain_output();
+        led_3.set_high().unwrap();
+
+        let mut global_timer = Timer::tim4(dp.TIM4, 1.hz(), clocks);
         global_timer.listen(stm32f4xx_hal::timer::Event::TimeOut);
 
         init::LateResources {
-            global_timer
+            global_timer,
+            led_2,
+            led_3
         }
     }
 
     #[idle]
     fn idle(_cx: idle::Context) -> ! {
-        stm32f4xx_hal::stm32::NVIC::pend(stm32f4xx_hal::stm32::Interrupt::EXTI0);
-
         loop {
             cortex_m::asm::nop();
         }
     }
 
-    #[task(binds = TIM4, resources = [global_timer])]
+    #[task(binds = TIM4, resources = [global_timer, led_2])]
     fn on_global_timer(cx: on_global_timer::Context) {
+        static mut COUNT: u32 = 0;
+
         let global_timer: &mut Timer<TIM4> = cx.resources.global_timer;
         global_timer.clear_interrupt(stm32f4xx_hal::timer::Event::TimeOut);
 
-        rprintln!("On global timer tick");
-    }
+        let led_2: &mut Led2Pin = cx.resources.led_2;
+        led_2.toggle().unwrap();
 
-    #[task(binds = EXTI0)]
-    fn exti(cx: exti::Context) {
-        rprintln!("exti");
+        rprintln!("On global timer tick: {}", COUNT);
+        *COUNT = COUNT.wrapping_add(1);
     }
 };
 
